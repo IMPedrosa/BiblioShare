18037ca03287e016d495677039e416de6ff6a702
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-10-27 18:52:54 -0300

    Create all database tables within app context.

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,17 +1,23 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models import db
 from routes import auth
 from config import Config
+from models import User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///biblioshare.db'
-db = SQLAlchemy(app)
 app.config.from_object(Config)
 
+db.init_app(app)
+
 app.register_blueprint(auth)
+
 @app.route('/')
 def index():
   return ""
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
 	app.run(debug=True)
