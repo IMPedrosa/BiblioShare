f3fb682391bcc52ffdabeab1bb59b4a56791e99d
Thiago Assis <thiagoassis.99@gmail.com>
2024-10-30 15:59:59 -0300

    add new model for Book table

--- backend/src/models.py
+++ backend/src/models.py
@@ -7,3 +7,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    author = db.Column(db.String(150), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    details = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
